b0ad813dc6a17f42e35c5431c2b0feecd01dc61c
Vivek7412 <vivek7061054537@gmail.com>
2024-01-15 14:08:02 +0530

    loops

--- loops/multiplication.js
+++ loops/multiplication.js
@@ -1,5 +1,7 @@
+// print the multiplication table 
+
 let n=prompt("Write your number");
 n=parseInt(n);
 for(let i=n; i<=n*10; i=i+n){
-    console.log(i);
+    console.log(n);
 }
---
+++ loops/multiplication1.js
@@ -0,0 +1,8 @@
+for(let i=5;i<=50;i=i+5){
+    console.log(i);
+}
+
+console.log("Table of 7");
+for(let i=7;i<=70;i=i+7){
+    console.log(i);
+}
