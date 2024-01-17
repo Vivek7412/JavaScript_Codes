c84b3700bda120c5639f73200e9306f58841035e
Vivek7412 <vivek7061054537@gmail.com>
2024-01-17 23:32:15 +0530

    js

---
+++ Functions/arg.js
@@ -0,0 +1,13 @@
+// Function with Arguments
+
+function printInfo(name,age){
+    console.log(`${name}'s age is ${age}.`);
+}
+printInfo("Vivek Kumar",23);
+printInfo("Sumit Raj");
+
+
+function sum(a, b) {
+    console.log(a+b);
+}
+sum(45,7);
---
+++ Functions/fun.js
@@ -0,0 +1,19 @@
+function fun() {
+    console.log("Hello!");
+}
+fun(); 
+function printName(){
+    console.log("Vivek Kumar");
+}
+printName();
+
+// function for loop
+function ptint1to5(){
+    for(let i=1; i<=5;i++){
+        console.log(i);
+    }
+}
+ptint1to5();
+
+ 
+
