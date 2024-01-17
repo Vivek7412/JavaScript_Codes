0d176bceed5f5a476aa1e75f9aac6b08e6c54089
Vivek7412 <vivek7061054537@gmail.com>
2024-01-17 22:34:09 +0530

    js

---
+++ Object_Literals/Qs1.js
@@ -0,0 +1,12 @@
+// GEnerate a random number between 1 to 100.
+
+Math.floor(Math.random()*100)+1;
+
+
+// Generate a random number between 1to 5.
+
+Math.floor(Math.random()*5 )+1;
+
+// Generate a random number between 20 to 25.
+
+Math.floor(Math.random()*5 )+20;
